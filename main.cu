ff4a01dbd466882b7bb8e27175a8e3d3d3d68e8a
Morgan Lytle <mlytle@bender.engr.ucr.edu>
2018-05-17 11:47:56 -0700

    initial main file

---
+++ main.cu
@@ -0,0 +1,25 @@
+#include <stdio.h> //Import libraries and files
+#include <stdlib.h>
+#include "kernel.cu"
+
+
+
+
+
+
+
+
+
+
+
+/*
+Allocate memory for the database on the host and the device
+*/
+
+
+/*
+random variable that is 0 or 1
+if 0 generate a random integer (0-9)
+if 1 generate a random letter(A-Z)
+fill two dimensional array with these characters
+*/
